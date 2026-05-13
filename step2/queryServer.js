6fdcfc1092830324b64db0e810ed53dac9cbf85f
ajah <ajahajang00@gmail.com>
2026-05-13 18:26:35 +0200

    chore: read and send query parasm

--- step2/queryServer.js
+++ step2/queryServer.js
@@ -1,22 +1,16 @@
 import http from 'http';
 import url from 'url'
-const server=http.createServer((req,res)=>{
-    
-    const parsedUrl={
-        pathname:'/',
-        query:{
-            name:'John'
-        }
-    }
-    if(parsedUrl.pathname==='/' && parsedUrl.query==='John'){
-        res.writeHead(200,{"Content-Type":'text/html'})
-            res.end('<h1>Hello John</h1>')
+const server = http.createServer((req, res) => {
+    const parSedurl = url.parse(req.url, true)
+
+    if (parSedurl.pathname === '/greet' && parSedurl.query.name==="John"){
+        res.writeHead(200, { "Content-Type": 'text/html' })
+res.end('<h1>Hello John</h1>')
         
     }
 
 
 })
-server.listen(3000,()=>{
+server.listen(3000, () => {
     console.log('Server listening on port 3000')
 })
-      
