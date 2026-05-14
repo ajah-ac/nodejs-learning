e25ab1893268bcf1543a3f627aeab78ebef78ea5
ajah <ajahajang00@gmail.com>
2026-05-14 17:25:08 +0200

    chore: add multiple routes

--- step3_express/app.js
+++ step3_express/app.js
@@ -3,6 +3,12 @@ const app=express()
 app.get('/',(req,res)=>{
     res.send('Hello')
 })
+app.get('/about',(req,res)=>{
+    res.send('<h1>This is the about page</h1>')
+})
+app.get('/contact',(req,res)=>{
+    res.send('<h1>This is the contact page</h1>')
+})
 
 app.listen(3000,()=>{
     console.log('Server is listening on port 3000')
