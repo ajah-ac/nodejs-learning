3672eaa43b1084974a43496aa99242885279907e
ajah <ajahajang00@gmail.com>
2026-05-14 17:11:27 +0200

    chore: return json data

--- step2/completeServer.js
+++ step2/completeServer.js
@@ -1,6 +1,16 @@
 import http from 'http';
 import url from 'url'
-
+const jsonData = [{
+    id: 1,
+    name: 'apple',
+    price: 200},{
+    id: 2,
+    name: 'orange',
+    price: 300},{
+    id: 1,
+    name: 'mango',
+    price: 100}
+]
 const server=http.createServer((req,res)=>{
   const parsedUrl=url.parse(req.url, true)
   const pathname=parsedUrl.pathname
@@ -18,8 +28,16 @@ const server=http.createServer((req,res)=>{
     res.end('Hello John')
   }
   else if (pathname==='/search'){
-      res.writeHead(200,{'content-type':'text-plain'})
+      res.writeHead(200,{'content-type':'text/plain'})
     res.end(`Search results for:${parsedUrl.query.q}`)
+  }
+  else if (pathname==='/api/products'){
+    res.writeHead(200,{'content-type':'application/json'})
+    res.end(JSON.stringify(jsonData))
+  }
+  else{
+    res.writeHead(404,{'content-type':'text/plain'})
+    res.end('404 - Page Not found')
   }
   
 })
