45ee94acf9f143f51a321cbb189d69cf8f985592
ajah <ajahajang00@gmail.com>
2026-05-14 17:30:26 +0200

    chore: add route parameters

--- step3_express/app.js
+++ step3_express/app.js
@@ -9,6 +9,14 @@ app.get('/about',(req,res)=>{
 app.get('/contact',(req,res)=>{
     res.send('<h1>This is the contact page</h1>')
 })
+app.get('/user/:id',(req,res)=>{
+    const id=req.params.id
+    res.send(`<h1> UserId is :${id}</h1>`)
+})
+app.get('/product/:name',(req,res)=>{
+    const name=req.params.name
+    res.send(`<h1> Product name is :${name}</h1>`)
+})
 
 app.listen(3000,()=>{
     console.log('Server is listening on port 3000')
