b5e4692f62acc91911f8df3674be0de450cdf874
ajah <ajahajang00@gmail.com>
2026-05-15 15:06:25 +0200

    chore: create an express app server

--- clustering/index.js
+++ clustering/index.js
@@ -1,7 +1,16 @@
 import express from 'express'
+import fs from 'fs/promises'
 const app=express()
-app.get('/',(req,res)=>{
-    res.send('Hello there')
+app.get('/',async (req,res)=>{
+    try {
+        const data=await fs.readFile('content.txt','utf-8')
+        res.status(200).send(data)
+    
+    } catch (error) {
+        console.log('Error reading file:',error)
+        res.status(500).send('Internal Server Error');
+    }
+    
 })
 app.listen(3000,()=>{
     console.log('Server is listening on port 3000')
