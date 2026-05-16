969785592ae1bb4e1524296e1ebff795d5e99173
ajah <ajahajang00@gmail.com>
2026-05-16 21:51:44 +0200

    chore: create a custom auth middleware

--- step4_routes_middleware/app.js
+++ step4_routes_middleware/app.js
@@ -1,11 +1,23 @@
 import express from 'express';
 
-const app=express()
 
+const app=express()
+const authMiddleware=(req,res,next)=>{
+   const token=req.headers.authorization
+   if(token){
+    next()
+   } 
+else{
+    res.status(401).send('Forbidden')
+}
+
+}
 app.use((req,res,next)=>{
     console.log(`Method:${req.method}, URL:${req.url}`)
     next()
 })
+
+
 app.listen(3000,()=>{
     console.log('Server is listening on port 3000')
 
