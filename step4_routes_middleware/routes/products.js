5b076c73354162889035b3472a639d9a9518a30d
ajah <ajahajang00@gmail.com>
2026-05-16 22:36:42 +0200

    chore: remove express app from routes

--- step4_routes_middleware/routes/products.js
+++ step4_routes_middleware/routes/products.js
@@ -1,5 +1,4 @@
 import express from 'express'
- const app=express()
 const router=express.Router()
 app.get('/products',(req,res)=>{
     res.status(200).send('Here are your products')
--- step4_routes_middleware/routes/users.js
+++ step4_routes_middleware/routes/users.js
@@ -1,5 +1,4 @@
 import express from 'express'
- const app=express()
 const router=express.Router()
 app.get('/users',(req,res)=>{
     res.status(200).send('User route')
