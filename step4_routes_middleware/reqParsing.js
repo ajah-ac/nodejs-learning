f6d202adfc1bcf30b6567ab750715ecef9996d15
ajah <ajahajang00@gmail.com>
2026-05-16 22:15:05 +0200

    chore: add middleware to parse request bodies

---
+++ step4_routes_middleware/reqParsing.js
@@ -0,0 +1,28 @@
+import express from 'express'
+const app=express()
+app.use((req,res,next)=>{
+    console.log(req.method,req.url)
+    next()
+})
+const data=[{
+    id:1,
+    name:'orange',
+    price:100
+}]
+app.use(express.json())
+app.post('/add-item',(req,res)=>{
+    const newItem={
+        id:Date.now(),
+        name:req.body.name,
+        price:req.body.price
+    }
+    data.push(newItem)
+        console.log(data)
+
+    res.json(data)
+})
+
+app.listen(3000,()=>{
+    console.log('Server is listening on Port 3000')
+
+})
