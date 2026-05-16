83fab687644e4e360b5eb52480fc6e5a7e826ec6
ajah <ajahajang00@gmail.com>
2026-05-16 22:31:15 +0200

    chore: add user routes

---
+++ step4_routes_middleware/routes/users.js
@@ -0,0 +1,8 @@
+import express from 'express'
+ const app=express()
+const router=express.Router()
+app.get('/users',(req,res)=>{
+    res.status(200).send('User route')
+})
+
+export default router
