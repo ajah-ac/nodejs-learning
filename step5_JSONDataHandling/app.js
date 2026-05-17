5a71e68ee5e9d3389a9a4067fb0708eef64801a7
ajah <ajahajang00@gmail.com>
2026-05-17 22:39:28 +0200

    chore: get products as JSON

---
+++ step5_JSONDataHandling/app.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import JSONRoutes from '../routes/step5Routes.js'
+
+const app=express();
+app.use(express.json())
+app.use('/api/products',JSONRoutes)
+app.listen(3000,()=>{
+    console.log('Server is listening on port 3000')
+})
