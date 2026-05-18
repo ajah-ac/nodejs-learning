a7a5220e0c1bd6aa8f9b4647844e1e1528cf04c1
ajah <ajahajang00@gmail.com>
2026-05-18 17:56:12 +0200

    chore: add update route

--- routes/step5Routes.js
+++ routes/step5Routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { validate } from '../middleware/step5.js';
-import { getProducts,createProducts, getProductByid } from '../controllers/step5.js';
+import { getProducts,createProducts, getProductByid, updateProduct } from '../controllers/step5.js';
 const router=express.Router();
 router.get('/',getProducts)
 router.post('/',validate,createProducts)
 router.get('/:id',getProductByid)
+router.put('/:id',updateProduct)
 export default router
