0687d4e7174bf099e7b4bf007ee6f97705ab2c40
ajah <ajahajang00@gmail.com>
2026-05-19 22:34:37 +0200

    chore: additional validation constraints

--- middleware/step5.js
+++ middleware/step5.js
@@ -1,11 +1,16 @@
 export const validate = (req, res, next) => {
-    const { name, price } = req.body;
+    const { name, price,description } = req.body;
+if(name===undefined || price===undefined ||description===undefined){
+    return res.status(400).send('Please fill in all fields')
+}
 
-    if (typeof name !== 'string' || name.trim() === '' ) {
+    if (typeof name !== 'string' || name.trim()==='') {
         return res.status(400).send('Invalid name');
     }
-
-    if (typeof price !== 'number') {
+if (typeof description !== 'string' || description.trim()==='') {
+        return res.status(400).send('Invalid description');
+    }
+    if (typeof price !== 'number' || price<0) {
         return res.status(400).send('Price must be a number');
     }
 
