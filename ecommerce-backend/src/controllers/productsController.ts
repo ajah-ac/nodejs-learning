78dfecc74996883cc9abff8ae59ce343e85510ed
ajah <ajahajang00@gmail.com>
2026-06-28 23:46:45 +0200

    chore: add products Controller

--- ecommerce-backend/src/controllers/productsController.ts
+++ ecommerce-backend/src/controllers/productsController.ts
@@ -1,69 +1,72 @@
-import { response, request } from 'express'
-import productsData from '../data/products.ts'
-import { getById } from '../data/utils/getById.ts';
-interface ProdParams{
-    category?:string,
-        minPrice?:number,
-    maxPrice?:number,
-    search?:string,
+import type { Response, Request } from 'express'
+import productsData from '../data/products'
+import { getById } from '../data/utils/getById.js';
+interface ProdParams {
+    category?: string,
+    minPrice?: number,
+    maxPrice?: number,
+    search?: string,
 
 }
-export const getAllProducts = (req:request<ProdParams>, res:response) => {
- const {category,minPrice, maxPrice, search}=req.query;
-if(req.query===undefined){
-    return res.status(200).json(productsData)
-}
-if(category && category.trim()!==''){
-    const filtered=productsData.filter(p=>p.category.toLowerCase()===category.toLowerCase())
-if(filtered.length===0){
-    return res.status(404).send(`No products exist in ${category} category` )
-}
- return res.status(200).json(filtered)
-}
-if(search && search.trim()!==''){
-       const filtered=productsData.filter(p=>(p.name.toLowerCase()|| p.description ).includes(search.toLowerCase()))
-if(filtered.length===0){
-    return res.status(404).send(`No products exist in ${category} category` )
-} 
- return res.status(200).json(filtered)
-
-}
-
-if(minPrice && maxPrice){
-    const filtered=productsData.filter(p=>p.price>=minPrice && p.price <=maxPrice)
-    if(filtered.length===0){
-        return res.status(200).json([])
+export const getAllProducts = (
+    req: Request<{}, {}, {}, ProdParams>,
+    res: Response
+) => {
+    const { category, minPrice, maxPrice, search } = req.query;
+    const min=minPrice? +minPrice : undefined
+    const max=maxPrice? +maxPrice : undefined
+    if (typeof category === 'string' && category.trim() !== '') {
+        const filtered = productsData.filter(p => p.category.toLowerCase() === category.toLowerCase())
+        if (filtered.length === 0) {
+            return res.status(404).send(`No products exist in ${category} category`)
+        }
+        return res.status(200).json(filtered)
     }
-    return res.status(200).json(filtered)
-}
-else if(minPrice){
-     const filtered=productsData.filter(p=>p.price >= minPrice)
-    if(filtered.length===0){
-        return res.status(200).json([]) 
-    }
-    return res.status(200).json(filtered)
-}
-else if(maxPrice){
-     const filtered=productsData.filter(p=>p.price >= maxPrice)
-    if(filtered.length===0){
-        return res.status(200).json([])
-    }
-    return res.status(200).json(filtered)
-}
-else{
-    res.status(200).json([])
-}
+    if (typeof search === 'string' && search.trim() !== '') {
+        const filtered = productsData.filter(p => p.name.toLowerCase().includes(search.toLowerCase()) || p.description.toLowerCase().includes(search.toLowerCase()))
+        if (filtered.length === 0) {
+            return res.status(404).send(`No products exist in ${category} category`)
+        }
+        return res.status(200).json(filtered)
 
-}
+    }
 
-type Id={
-id:number
-}
-export const getProductByid=(req:request<Id>,res:response)=>{
-    const id=+req.params
-    const product=getById(productsData,id)
-    if(product===undefined){
+    if (min !== undefined && max !== undefined) {
+        const filtered = productsData.filter(p => p.price >= min && p.price <= max)
+        if (filtered.length === 0) {
+            return res.status(200).json([])
+        }
+        return res.status(200).json(filtered)
+    }
+    else if (min !== undefined && max === undefined) {
+        const filtered = productsData.filter(p => p.price >= min)
+        if (filtered.length === 0) {
+            return res.status(200).json([])
+        }
+        return res.status(200).json(filtered)
+    }
+    else if (max !== undefined && min === undefined) {
+        const filtered = productsData.filter(p => p.price <= max)
+        if (filtered.length === 0) {
+            return res.status(200).json([])
+        }
+        return res.status(200).json(filtered)
+    }
+    else {
         res.status(200).json([])
+    }
+     res.status(200).json(productsData)
+
+}
+
+type Id = {
+    id: number
+}
+export const getProductByid = (req: Request<Id>, res: Response) => {
+    const id = +req.params.id
+    const product = getById(productsData, id)
+    if (product === undefined) {
+        return res.status(404).json({ error: 'Product not found' })
     }
     return res.status(200).json(product)
 }
--- ecommerce-backend/src/routes/products.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { getAllProducts } from '../controllers/productsController.ts'
- const router=express.Router()
-router.get('/',getAllProducts)
-router.post('/')
-router.put('/:id')
-router.delete('/:id')
-export default router
